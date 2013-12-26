c1f886bfddae9ef816d4a1f42511ee4b50faca73
Kriks <K.Shpakovski@itechart-group.com>
2013-12-26 17:35:17 +0300

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,15 +1,26 @@
 include ApplicationHelper
 
-def valid_signin(user)
+def sign_in(user)
+  visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # Вход без Capybara.
+  #cookies[:remember_token] = user.remember_token
+
 end
 def provide_valid_signup_data
   fill_in "Name",         with: "Example User"
   fill_in "Email",        with: "user@example.com"
   fill_in "Password",     with: "foobar"
   fill_in "Confirmation", with: "foobar"
+end
+
+def provide_new_valid_signup_data
+  fill_in "Name",             with: new_name
+  fill_in "Email",            with: new_email
+  fill_in "Password",         with: user.password
+  fill_in "Confirmation", with: user.password
 end
 
 RSpec::Matchers.define :have_no_error_message do
